da2199ee1627bcd5b574b41da79fb0dcc230f2aa
Manuel Aristarán <jazzido@jazzido.com>
2017-02-16 19:35:39 -0500

    cache cubes in client

--- src/client.ts
+++ src/client.ts
@@ -8,19 +8,28 @@ import Aggregation from './aggregation';
 export default class Client {
 
     private api_base: string;
+    private cubesCache: { [cname: string]: Cube };
 
     constructor(api_base: string) {
         this.api_base = api_base;
+        this.cubesCache = {};
     }
 
     cube(name: string): Promise<Cube> {
-        return axios.get(urljoin(this.api_base, 'cubes', name))
-            .then((value: AxiosResponse) => {
-                return Cube.fromJSON(value.data)
-            });
+        if (name in this.cubesCache) {
+            return Promise.resolve(this.cubesCache[name]);
+        }
+        else {
+            return axios.get(urljoin(this.api_base, 'cubes', name))
+                .then((value: AxiosResponse) => {
+                    const c = Cube.fromJSON(value.data);
+                    this.cubesCache[c.name] = c;
+                    return c;
+                });
+        }
     }
 
-    query(query: Query) {
+    query(query: Query): Promise<Aggregation> {
         return axios
             .get(urljoin(this.api_base, query.path()))
             .then((value) => {
