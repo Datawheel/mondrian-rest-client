ccd8409fa5aff66b0e8ed69766649ff351b6abef
Manuel Aristaran <manuel@datawheel.us>
2017-06-30 19:55:13 -0400

    adjust assertion

--- test/client.js
+++ test/client.js
@@ -19,6 +19,7 @@ describe('Cube', function() {
                                                'caption',
                                                'measures',
                                                'dimensions',
+                                               'namedSets',
                                                'dimensionsByName' ]);
         });
     });
